8913b51290f6aac310f1d53ee7b1fe3548f6727f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-04-28 15:39:38 +0200

    Change dialog title for new observation
    Updated dialog title for new observation.

--- home.py
+++ home.py
@@ -539,7 +539,7 @@ def login_view():
 
 # ---------- DIALOG FOR NEW OBSERVATION ----------
 
-@st.dialog("Add new observation",width="large")
+@st.dialog("",width="large")
 def new_observation_dialog(user):
     st.write("Drag the marker to the correct location and fill in the details.")
 
