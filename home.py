a18d97c9f5f387f5cc88299a47b3a357886e252f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-04-28 17:52:24 +0200

    Fix condition to check map_state for last object

--- home.py
+++ home.py
@@ -557,7 +557,7 @@ def main_app(user: dict):
     )
 
     # Store last clicked location as map_center (for default marker position)
-    if map_state and map_state.get("last_object_clicked"):
+    if map_state or map_state.get("last_object_clicked"):
         loc = map_state["center"]
         st.session_state["map_center"] = [loc.get("lat"), loc.get("lng")]
 
