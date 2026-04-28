d93d554836e5dfd85cb3a2cc5294d8eb4371822a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-04-28 12:28:44 +0200

    Update home.py

--- home.py
+++ home.py
@@ -16,10 +16,11 @@ import uuid
 
 # --- CONFIG ---
 st.set_page_config(page_title="Geo Observations", layout="wide")
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+
 
 # --- SESSION LOGIN ---
 if "user" not in st.session_state:
