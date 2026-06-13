59342c82be9b3117ef6b97b35a1310fde71879f3
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-06-13 19:00:37 +0200

    Update home.py

--- home.py
+++ home.py
@@ -266,7 +266,7 @@ def dagverslagen_overview():
             .sort_values("date")
             .groupby(["project", "report_type"])
             .apply(lambda x: " | ".join([
-                f"• {row['kind']} : {pd.to_datetime(row['date']).strftime('%d %B %Y')}"
+                f"{row['kind']} : {pd.to_datetime(row['date']).strftime('%d %B %Y')}"
                 for _, row in x.iterrows()
             ]))
             .reset_index(name="details_text")
@@ -315,6 +315,7 @@ def dagverslagen_overview():
             file_name="alle_dagverslagen.csv",
             mime="text/csv",
         )
+
 
 
 
