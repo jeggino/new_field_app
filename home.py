ba3b665b9195b433dec11ee1e8c89e4668466727
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-08-01 21:30:56 +0200

    Update home.py

--- home.py
+++ home.py
@@ -91,7 +91,7 @@ FUNCTION_ICONS = {
     "nestlocatie": "egg",
     "mogelijke nestlocatie": "question",
 
-    "plant": "fa-pagelines"
+    "plant": "fa-light fa-leaf"
 }
 
 # ----------------- COLORS FOR SPECIES -----------------
