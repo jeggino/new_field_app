e4ea2085a86baa00a0acbdeebcf0ca233697d73a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-08-02 22:48:21 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1397,7 +1397,7 @@ def show_project_selection():
 # ----------------- MAIN APP -----------------
 def show_main_app():
     # NO title on main page, only New Observation button (mobile-friendly)
-    with st.popover("New Observation",width="stretch",icon=":material/add_circle:"):
+    with st.expander("New Observation",width="stretch",icon=":material/add_circle:"):
         col1, col2 = st.columns([0.5, 0.5])
 
 
