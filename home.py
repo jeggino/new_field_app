9bcd640f0516c13a3af271b9c8ccc54522bd1477
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-08-02 19:45:38 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1725,10 +1725,11 @@ def show_main_app():
     if map_data and map_data.get("last_active_drawing"):
         if map_data["last_active_drawing"]["geometry"]["type"] == "Polygon":
             st.write(map_data["last_active_drawing"])
-            st.stop()
-            # if st.sidebar.button(label, key=f"obs_{obs_id}", use_container_width=True):
-            #     edit_polygon_dialog(map_data["last_active_drawing"]["properties"]["id"])
-            #     st.stop()
+            label = f"{map_data["last_active_drawing"]["properties"]["species"]} - {map_data["last_active_drawing"]["properties"]["function"]}"
+            obs_id = f"{map_data["last_active_drawing"]["properties"]["id"]}"
+            if st.sidebar.button(label, key=f"obs_{obs_id}", use_container_width=True):
+                edit_polygon_dialog(map_data["last_active_drawing"])
+                st.stop()
 
         elif map_data["last_active_drawing"]["geometry"]["type"] == "Point":
 
