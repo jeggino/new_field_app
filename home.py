e4056a515ec5c1e44c3759dabb9540bac47cb9c9
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-08-02 20:21:15 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1729,13 +1729,8 @@ def show_main_app():
     # Use last_object_clicked_popup from st_folium
     if map_data and map_data.get("last_active_drawing"):
         if map_data["last_active_drawing"]["geometry"]["type"] == "Polygon":
-            st.write(map_data["last_active_drawing"])
             obs_id = f"{map_data["last_active_drawing"]["properties"]["id"]}"
             label = f"({obs_id}) {map_data["last_active_drawing"]["properties"]["species"]} - {map_data["last_active_drawing"]["properties"]["function"]}"
-            
-            st.write(label)
-            st.write(obs_id)
-            # st.sidebar.button(label, key=f"obs_{obs_id}", use_container_width=True)
             if st.sidebar.button(label, key=f"obs_{obs_id}", use_container_width=True):
                 edit_polygon_dialog(map_data["last_active_drawing"])
                 st.stop()
