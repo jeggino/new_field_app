1ee5caeb0d5c98aa8c2758a6b70391122a5eb11a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-08-04 17:06:12 +0200

    Refactor New observation section layout
    Refactor observation buttons into a container for better layout.

--- home.py
+++ home.py
@@ -2219,11 +2219,12 @@ def show_main_app():
     st.session_state.map_input_center = _get_center_from_map_data(map_data, st.session_state.map_center)
     
     with col1:
-        st.markdown(":green[**New observation**]",text_alignment='center')
-        if st.button("New Point",key="New Observation",width="stretch",icon=":material/add_location_alt:"):
-            new_observation_dialog(st.session_state.map_input_center)
-        if st.button("New Polygon", key="New Polygon",width="stretch",icon=":material/screenshot_region:"):
-            new_polygon_dialog(st.session_state.map_input_center)
+        with st.container(":green[**New observation**]"):
+        # st.markdown(":green[**New observation**]",text_alignment='center')
+            if st.button("New Point",key="New Observation",width="stretch",icon=":material/add_location_alt:"):
+                new_observation_dialog(st.session_state.map_input_center)
+            if st.button("New Polygon", key="New Polygon",width="stretch",icon=":material/screenshot_region:"):
+                new_polygon_dialog(st.session_state.map_input_center)
 
         st.divider()
     
