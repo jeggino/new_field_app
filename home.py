7901c5e2f86240a92c68f6073f318df248ed04db
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-08-04 14:54:13 +0200

    Update home.py

--- home.py
+++ home.py
@@ -2026,11 +2026,12 @@ def show_main_app():
             },
         }
 
-        species_short = SPECIES_SHORT[species]
-        function_short = FUNCTION_SHORT[function_type]
+        # species_short = SPECIES_SHORT[species]
+        # function_short = FUNCTION_SHORT[function_type]
         
-        layer_name = f"{species_short}-{function_short} ({id_type})"
-        fg = folium.FeatureGroup(name=layer_name)
+        # layer_name = f"{species_short}-{function_short} ({id_type})"
+        # fg = folium.FeatureGroup(name=layer_name)
+        fg = folium.FeatureGroup(name="peppe")
         
         geojson = folium.GeoJson(
             feature,
