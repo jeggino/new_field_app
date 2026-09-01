b2a1537a0add7c85bc2336b9576775e291d21ff7
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-09-01 12:46:42 +0200

    Update text description for bat observations

--- home.py
+++ home.py
@@ -3391,7 +3391,7 @@ elif page == "Gegenereerde output":
     st.markdown(
         """
         <p style='font-size:16px; color:#555; margin-top:0.2rem;'>
-            Waarnemingen en aantallen van vleermuizen gedurende de veldbezoeken in het onderzoeksgebied.
+            Waarnemingen en aantallen van vleermuizen gedurende de veldbezoeken.
         </p>
         """,
         unsafe_allow_html=True
