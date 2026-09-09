c12b838a61758a3bf4bea38d4b9ce8b73877d042
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-09-09 10:30:57 +0200

    Replace success message with markdown format

--- home.py
+++ home.py
@@ -4608,12 +4608,8 @@ elif page == "Gegenereerde output":
     
     if upload_response.status_code in [200, 201]:
         github_url = f"https://{USERNAME}.github.io/{REPO}/{FILE_PATH}"
-        # st.success(f"De kaart is met succes naar de GitHub‑repository geüpload. Klik op deze [link]({github_url}) om de kaart te bekijken en te delen als je dat wilt.")
-        st.success(
-            f"""De kaart is met succes naar de GitHub‑repository geüpload. 
-            Klik op deze <a href="{github_url}" target="_blank" 
-            style="color:red; font-weight:bold;">link</a> om de kaart te bekijken en te delen als je dat wilt."""
-        )
+        st.markdown(f"De kaart is met succes naar de GitHub‑repository geüpload. Klik op deze [link]({github_url}) om de kaart te bekijken en te delen als je dat wilt.")
+
 
 
     else:
