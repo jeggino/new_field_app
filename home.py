ac24e4c04223fd1a054b96fcc01e5e7e776a1f5d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2026-09-09 12:05:32 +0200

    Update home.py

--- home.py
+++ home.py
@@ -4558,7 +4558,7 @@ elif page == "Gegenereerde output":
     GITHUB_TOKEN = st.secrets["GITHUB_TOKEN"]   # store token in Streamlit secrets
     USERNAME = "jeggino"
     REPO = "EE_HTML_particulieren"
-    BRANCH = "main"
+    BRANCH = "gh-pages"
     FILE_PATH = f"{safe_project_name}_HTML.html"   # or "maps/map.html"
 
     
